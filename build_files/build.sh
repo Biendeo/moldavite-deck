309033a2381d843ed8239e7fceaff268224e7908
Biendeo <biendeo@biendeo.com>
2026-06-03 06:55:32 +1000

    Removed line to install kernel development libraries.

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,8 +8,6 @@ dnf5 install -y wireshark
 chgrp wireshark /usr/sbin/dumpcap
 chmod o-rx /usr/sbin/dumpcap
 
-rpm-ostree install kernel-devel-$(uname -r) gcc make
-
 cd /tmp
 wget https://raw.githubusercontent.com/OpenZotacZone/ZotacZone-Drivers/main/install_openzone_drivers.sh
 chmod +x install_openzone_drivers.sh
