cd9e224500886c9cf5b3aa6c159479aa368f41a4
Biendeo <biendeo@biendeo.com>
2026-06-03 06:38:02 +1000

    Added Openzone drivers.

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,3 +7,11 @@ set -ouex pipefail
 dnf5 install -y wireshark
 chgrp wireshark /usr/sbin/dumpcap
 chmod o-rx /usr/sbin/dumpcap
+
+rpm-ostree install kernel-devel-$(uname -r) gcc make
+
+cd /tmp
+wget https://raw.githubusercontent.com/OpenZotacZone/ZotacZone-Drivers/main/install_openzone_drivers.sh
+chmod +x install_openzone_drivers.sh
+echo y | sudo ./install_openzone_drivers.sh
+rm install_openzone_drivers.sh
