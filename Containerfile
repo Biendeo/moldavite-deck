fe8c8a6551b8cf74cfd40cd0141657698c2696e9
Biendeo <biendeo@biendeo.com>
2026-07-04 19:56:02 +1000

    Upgraded Bazzite to testing-20260704 and removed Openzone drivers.

--- Containerfile
+++ Containerfile
@@ -4,7 +4,7 @@ COPY build_files /
 
 # Base Image
 # Check upstream from https://github.com/ublue-os/bazzite/pkgs/container/bazzite-deck/versions?filters[version_type]=tagged
-FROM ghcr.io/ublue-os/bazzite-deck:stable-43.20260420
+FROM ghcr.io/ublue-os/bazzite-deck:testing-44.20260704
 
 ## Other possible base images include:
 # FROM ghcr.io/ublue-os/bazzite:latest
--- build_files/build.sh
+++ build_files/build.sh
@@ -8,8 +8,8 @@ dnf5 install -y wireshark
 chgrp wireshark /usr/sbin/dumpcap
 chmod o-rx /usr/sbin/dumpcap
 
-cd /tmp
-wget https://raw.githubusercontent.com/OpenZotacZone/ZotacZone-Drivers/main/install_openzone_drivers.sh
-chmod +x install_openzone_drivers.sh
-echo y | sudo ./install_openzone_drivers.sh
-rm install_openzone_drivers.sh
+# cd /tmp
+# wget https://raw.githubusercontent.com/OpenZotacZone/ZotacZone-Drivers/main/install_openzone_drivers.sh
+# chmod +x install_openzone_drivers.sh
+# echo y | sudo ./install_openzone_drivers.sh
+# rm install_openzone_drivers.sh
