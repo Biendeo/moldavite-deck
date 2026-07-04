df59c0f558c8f4b342f490e296a67b91a8efc65b
Biendeo <biendeo@biendeo.com>
2026-07-04 21:08:40 +1000

    Installed HHD into the image.

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,6 +8,10 @@ dnf5 install -y wireshark
 chgrp wireshark /usr/sbin/dumpcap
 chmod o-rx /usr/sbin/dumpcap
 
+dnf5 copr enable hhd-dev/hhd
+dnf5 install hhd adjustor hhd-ui
+systemctl enable hhd@$(whoami)
+
 # cd /tmp
 # wget https://raw.githubusercontent.com/OpenZotacZone/ZotacZone-Drivers/main/install_openzone_drivers.sh
 # chmod +x install_openzone_drivers.sh
